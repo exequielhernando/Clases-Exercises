546ca1c9f12c066f997476381f94bd3ab0403d03
Exequiel Hernando <exequielhernando@gmail.com>
2020-07-09 19:28:44 -0300

    first practices with arrays

--- Clases Exercises/Clases Exercises/Program.cs
+++ Clases Exercises/Clases Exercises/Program.cs
@@ -15,6 +15,26 @@ namespace Clases_Exercises
             Calculator calculator = new Calculator();
             var result = calculator.Add(1, 2);
             Console.WriteLine(result);
+
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[1]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
