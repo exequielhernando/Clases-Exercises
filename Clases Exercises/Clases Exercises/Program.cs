a1d9f9a5fad3402d3632d54d20f2529897125fbc
Exequiel Hernando <exequielhernando@gmail.com>
2020-07-09 18:24:39 -0300

    Test for a simple clases

---
+++ Clases Exercises/Clases Exercises/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Clases_Exercises
+{   
+    public class Person
+    {
+        public String FirstName;
+        public String LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exequiel = new Person();
+            exequiel.FirstName = "Exequiel";
+            exequiel.LastName = "Hernando";
+            exequiel.Introduce();
+        }
+    }
+}
