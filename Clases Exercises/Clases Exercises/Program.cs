dfc4d704500500ae27aa5ce6fe7a8deadf95fd62
Exequiel Hernando <exequielhernando@gmail.com>
2020-07-15 18:53:13 -0300

    Working with enums examples

--- Clases Exercises/Clases Exercises/Program.cs
+++ Clases Exercises/Clases Exercises/Program.cs
@@ -1,40 +1,28 @@
-﻿using Clases_Exercises.Math;
-using System;
+﻿using System;
 
 namespace Clases_Exercises
 {
     class Program
-    {
+    {   public enum ShippingMethod
+        {
+            RegularAirMail = 1,
+            RegisteredAinMail = 2,
+            Express = 3
+        }
+
         static void Main(string[] args)
         {
-            var exequiel = new Person();
-            exequiel.FirstName = "Exequiel";
-            exequiel.LastName = "Hernando";
-            exequiel.Introduce();
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
 
-            var numbers = new int[3];
-            numbers[0] = 1;
+            Console.WriteLine(method.ToString());
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[1]);
-
-            var flags = new bool[3];
-            flags[0] = true;
-
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
-
-            var names = new string[3] { "Jack", "John", "Mary" };
-
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
         }
     }
 }
