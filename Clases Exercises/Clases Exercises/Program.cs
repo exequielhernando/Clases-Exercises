7aad358a5ee9e1821e25a8b4d31cd99ebb67845c
Exequiel Hernando <exequielhernando@gmail.com>
2020-07-15 17:58:33 -0300

    Working with strings

--- Clases Exercises/Clases Exercises/Program.cs
+++ Clases Exercises/Clases Exercises/Program.cs
@@ -1,5 +1,4 @@
-﻿using Clases_Exercises.Math;
-using System;
+﻿using System;
 
 namespace Clases_Exercises
 {
@@ -7,34 +6,14 @@ namespace Clases_Exercises
     {
         static void Main(string[] args)
         {
-            var exequiel = new Person();
-            exequiel.FirstName = "Exequiel";
-            exequiel.LastName = "Hernando";
-            exequiel.Introduce();
+            var firsName = "Exequiel";
+            var lastName = "Hernando";
+          
+            var fullName = firsName + " " + lastName;
+          
+            var myFullName = String.Format("My name is {0} {1}", firsName, lastName);
+            Console.WriteLine(myFullName);    
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
-
-            var numbers = new int[3];
-            numbers[0] = 1;
-
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[1]);
-
-            var flags = new bool[3];
-            flags[0] = true;
-
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
-
-            var names = new string[3] { "Jack", "John", "Mary" };
-
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
         }
     }
 }
