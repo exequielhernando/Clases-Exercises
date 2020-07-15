3cf36bc5bc42b18ac5fbd7db987b8401ae929570
Exequiel Hernando <exequielhernando@gmail.com>
2020-07-15 18:05:24 -0300

    More from String details

--- Clases Exercises/Clases Exercises/Program.cs
+++ Clases Exercises/Clases Exercises/Program.cs
@@ -12,8 +12,17 @@ namespace Clases_Exercises
             var fullName = firsName + " " + lastName;
           
             var myFullName = String.Format("My name is {0} {1}", firsName, lastName);
-            Console.WriteLine(myFullName);    
+            Console.WriteLine(myFullName);
 
+            var names = new String[3] { "John", "Jack", "Mary" };
+            var formattedNames = String.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look into the followins paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
         }
     }
 }
