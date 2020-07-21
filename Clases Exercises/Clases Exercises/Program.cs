3c0642c7022e9d106acdd321133606c71ca974ec
Exequiel Hernando <exequielhernando@gmail.com>
2020-07-21 18:43:13 -0300

    Cleaning the repo

--- Clases Exercises/Clases Exercises/Program.cs
+++ Clases Exercises/Clases Exercises/Program.cs
@@ -1,5 +1,4 @@
-﻿using Clases_Exercises.Math;
-using System;
+﻿using System;
 
 namespace Clases_Exercises
 {
@@ -7,34 +6,7 @@ namespace Clases_Exercises
     {
         static void Main(string[] args)
         {
-            var exequiel = new Person();
-            exequiel.FirstName = "Exequiel";
-            exequiel.LastName = "Hernando";
-            exequiel.Introduce();
-
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
-
-            var numbers = new int[3];
-            numbers[0] = 1;
-
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[1]);
-
-            var flags = new bool[3];
-            flags[0] = true;
-
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
-
-            var names = new string[3] { "Jack", "John", "Mary" };
-
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
+            Console.WriteLine("Hello world");
         }
     }
 }
